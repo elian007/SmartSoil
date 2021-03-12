189e650fbbd66d518d55db6e975360f9c09a0e27
Anderson Willian Santos Tonon <andersontonon@users.noreply.github.com>
2021-03-12 20:17:13 -0300

    ANDERSON TONON - CRIANDO FÓRMULA PARA CALCULO DE IDEAIS

---
+++ src/main/java/Codigo/CalculosIdeais.java
@@ -0,0 +1,40 @@
+
+package Codigo;
+
+public class CalculosIdeais {
+    
+    public float Fosforo(float valor){
+        
+        return 0;
+    }
+    
+    public float Potassio(float valor){
+        
+        return 0;
+    }
+        
+    public float Calcio (float valor){
+        
+        return 0;
+    }
+            
+    public float Magnesio(float valor){
+        
+        return 0;
+    }
+                
+    public float Enxofre(float valor){
+        
+        return 0;
+    }
+                    
+    public float Aluminio(float valor){
+        
+        return 0;
+    }
+    public float HAL(float valor){
+        
+        return 0;
+    }                
+    
+}
--- src/main/java/Codigo/Main.java
+++ src/main/java/Codigo/Main.java
@@ -2,5 +2,8 @@
 package Codigo;
 
 public class Main {
+    public static void main(String[] args) {
+        
+    }
     
 }
