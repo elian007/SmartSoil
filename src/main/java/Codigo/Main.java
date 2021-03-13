c4713b6d5c76b1ae6649ac563671d33e261d1ca9
Anderson Willian Santos Tonon <andersontonon@users.noreply.github.com>
2021-03-12 21:53:07 -0300

    ANDERSON TONON - 1º TESTE

--- src/main/java/Codigo/Main.java
+++ src/main/java/Codigo/Main.java
@@ -2,7 +2,15 @@
 package Codigo;
 
 public class Main {
+    
     public static void main(String[] args) {
+        
+        CalculosIdeais ideais = new CalculosIdeais(); 
+        
+        //Teste System.out.println(ideais.Enxofre(1));
+        System.out.println(ideais.Magnesio(1));
+
+
         
     }
     
