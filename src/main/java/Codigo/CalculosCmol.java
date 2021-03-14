28b557009b4f0bab8bb510195044da5794657eaa
Anderson Willian Santos Tonon <andersontonon@users.noreply.github.com>
2021-03-14 14:30:29 -0300

    ANDERSON TONON - ADICIONANDO CLASSE CALCULOSCMOL
    Calculos 
    S cmol
    CTC cmol
    V% atual

---
+++ src/main/java/Codigo/CalculosCmol.java
@@ -0,0 +1,30 @@
+
+package Codigo;
+
+public class CalculosCmol {
+        public float S_cmol(float potassio, float magnesio, float calcio){
+            
+            float soma = potassio + magnesio + calcio;
+            
+            return soma; 
+        }
+        
+        public float CTC_cmol(float potassio, float magnesio, float calcio, float h_al){
+            
+            float soma = potassio + magnesio + calcio + h_al;
+            
+            return soma;
+        
+        }
+        
+        public float V_atual (float potassio, float magnesio, float calcio, float h_al){
+            
+            float S_cmol = potassio + magnesio + calcio;
+            float CTC_cmol = potassio + magnesio + calcio + h_al;
+            float soma = 100 * S_cmol / CTC_cmol;
+            
+            return soma;
+        
+        }
+    
+}
