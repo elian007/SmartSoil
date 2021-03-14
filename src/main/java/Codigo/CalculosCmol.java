494e8fbcb9110446b190a82cb3753c9b0fba2973
Anderson Willian Santos Tonon <andersontonon@users.noreply.github.com>
2021-03-14 15:24:19 -0300

    ANDERSON TONON - TESTE DA CLASSE CALCULOSCMOL
    Teste S cmol
    Teste CTC cmol
    Teste V% atual

--- src/main/java/Codigo/CalculosCmol.java
+++ src/main/java/Codigo/CalculosCmol.java
@@ -2,22 +2,22 @@
 package Codigo;
 
 public class CalculosCmol {
-        public float S_cmol(float potassio, float magnesio, float calcio){
+        public float S_cmol(float potassio, float calcio, float magnesio){
             
             float soma = potassio + magnesio + calcio;
             
             return soma; 
         }
         
-        public float CTC_cmol(float potassio, float magnesio, float calcio, float h_al){
+        public float CTC_cmol(float potassio, float calcio, float magnesio, float h_al){
             
             float soma = potassio + magnesio + calcio + h_al;
             
-            return soma;
+            return  soma;
         
         }
         
-        public float V_atual (float potassio, float magnesio, float calcio, float h_al){
+        public float V_atual (float potassio, float calcio, float magnesio, float h_al){
             
             float S_cmol = potassio + magnesio + calcio;
             float CTC_cmol = potassio + magnesio + calcio + h_al;
--- src/main/java/Codigo/Main.java
+++ src/main/java/Codigo/Main.java
@@ -4,13 +4,21 @@ package Codigo;
 public class Main {
     
     public static void main(String[] args) {
-        
+        //-----------Teste ideais---------------------
         CalculosIdeais ideais = new CalculosIdeais(); 
         
         //Teste System.out.println(ideais.Enxofre(1));
-        System.out.println(ideais.Magnesio(1));
-
-
+        //Teste System.out.println(ideais.Magnesio(1));
+        
+        //--------------------------------------------
+        //-----------Teste cmol---------------------
+        CalculosCmol cmol = new CalculosCmol(); 
+        
+        //System.out.println(cmol.S_cmol((float)0.15,(float)5.76,(float)1.63));
+        //System.out.println(cmol.CTC_cmol((float)0.15,(float)5.76,(float)1.63, (float)5.35));
+        System.out.println(cmol.V_atual((float)0.15,(float)5.76,(float)1.63, (float)5.35));
+        
+        //--------------------------------------------
         
     }
     
