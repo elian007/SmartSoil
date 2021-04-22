1faffc6fb9a51ea1789100665c4e78b60102f9ea
Anderson Tonon <awst@live.com>
2021-04-21 23:53:53 -0300

    ANDERSON TONON - PEQUENA MELHORIA NA CLASSE CORREÇÃO RECUPERAÇÃO POTÁSSIO

--- src/main/java/Codigo/CorrecaoRecuperacaoPotassio.java
+++ src/main/java/Codigo/CorrecaoRecuperacaoPotassio.java
@@ -22,31 +22,20 @@ public class CorrecaoRecuperacaoPotassio {
     }
     
     public double CorrecaoRecuperacaoPotassio(int tipoFonte, double potassioDesejado, double potassio, double calcio, double magnesio,  double h_al){
-            
-        
+                 
         double tipoPotacio = tipoPotacio(tipoFonte);
         
-        
-                
         double soma1 = calcio + magnesio + potassio + h_al;
-        
         
         double soma2 = potassio / soma1 * 100;
         
         double soma3 = (potassio * potassioDesejado / soma2) - potassio;
         
-        double soma4 = soma3 * 39.1 * 10;
+        double soma4 = soma3 * 39.1 * 10 * 2 * 1.2  * 100 / 85 / 100;
         
-        double soma5 = soma4 * 2;
+        double soma5 = soma4 * 100 / tipoPotacio; 
         
-        double soma6 = soma5 * 1.2;
-        
-        double soma7 = soma6  * 100 / 85 / 100;
-        
-        double soma8 = soma7 * 100 / tipoPotacio; 
-        
-            
-        return (soma8 * 100) > 0 ? (soma8 * 100) : 0;
+        return (soma5 * 100) > 0 ? (soma5 * 100) : 0;
     }
     
 }
